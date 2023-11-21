78af2d36cf16a00ceaab5cd515b79b20b28f75f7
ShubhaM <81143367+shubham-1319@users.noreply.github.com>
2023-11-21 14:43:59 +0530

    Update code.cpp
    This code is to calculate the Gross Pay of an Employee where :-
    D.A. is 0.4%
    H.R.A is 0.2%
    
    These are predefined in the code

--- code.cpp
+++ code.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!";
+// calcularte gross salary
+#include <stdio.h>
+void main()
+{
+    float bp,da,hra,grpay;
+    printf("\nEnter the Basic pay:");
+    scanf("%f",&bp);
+    da=0.4*bp;
+    hra=0.2*bp;
+    grpay=bp+da+hra;
+    printf("Basic salry =%f\n",bp);
+    printf("darness allounce =%f\n",da);
+    printf("House rent allowance =%f\n",hra);
+    printf("Gross pay %f\n",grpay);
     return 0;
+
 }
